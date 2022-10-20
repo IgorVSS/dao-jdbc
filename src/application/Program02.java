7cccef57d8483b64e2cdbfcc995886447fc4dcec
Igor <bealmont@gmail.com>
2022-10-20 11:37:41 -0300

    department update, findById, findAll, deleteById

--- src/application/Program02.java
+++ src/application/Program02.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.List;
+import java.util.Scanner;
 
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
@@ -8,11 +10,39 @@ import model.entities.Department;
 public class Program02 {
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+	
 		DepartmentDao departmentDao = DaoFactory.createDepartmentDao();
-		System.out.println("=== TEST 4: department insert ===");
+		
+		System.out.println("=== TEST 1: department insert ===");
 		Department department = new Department(null, "Smartphone");
 		departmentDao.insert(department);
 		System.out.println("Inserted! New id = " + department.getId());
+		
+		System.out.println("=== TEST 2: department update ===");
+	    department = departmentDao.finById(3);
+		department.setName("Peripherals");
+		departmentDao.update(department);
+		System.out.println("Update completed!");
+		
+		System.out.println("=== TEST 3: department findById ===");
+		department = departmentDao.finById(1);
+		System.out.println("The id chosen was: " + department);
+		
+		System.out.println("=== TEST 4: department findAll ===");
+		List<Department> list = departmentDao.findAll();
+		
+		for(Department obj : list ) {
+			System.out.println(obj);
+		}
+		
+		System.out.println("=== TEST 5: department deleteById ===");
+		System.out.print("Enter ID for delete test: ");
+		int id = sc.nextInt();
+		departmentDao.deleteById(id);
+		System.out.println("Delete completed!");
+		sc.close();
+		
 		
 	}
 }
--- src/model/dao/impl/DepartmentDaoJDBC.java
+++ src/model/dao/impl/DepartmentDaoJDBC.java
@@ -5,6 +5,9 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
 
 import db.DB;
 import db.DbException;
@@ -57,20 +60,104 @@ public class DepartmentDaoJDBC implements DepartmentDao {
 
 	@Override
 	public void update(Department obj) {
+		PreparedStatement st = null;
 		
+		try {
+			st = conn.prepareStatement("Update department\r\n"
+					+ "SET Name = ?\r\n"
+					+ "WHERE Id = ?\r\n");
+			
+			st.setString(1, obj.getName());
+			st.setInt(2, obj.getId());
+			
+			st.executeUpdate();	
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		
 	}
 
 	@Override
 	public void deleteById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;	
 		
+		try {
+			st = conn.prepareStatement("DELETE FROM department WHERE Id = ?");
+
+			st.setInt(1, id);
+			int rowsAffected = st.executeUpdate();
+			if(rowsAffected == 0) {
+				throw new DbException("ID não encontrado!");
+			}
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
+		return;
 	}
 
 	@Override
 	public Department finById(Integer id) {
-		// TODO Auto-generated method stub
+		PreparedStatement st = null;	
+		ResultSet rs = null;
+		try {
+			
+			st = conn.prepareStatement("SELECT * FROM department WHERE Id = ?");
+
+			st.setInt(1, id);
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				return obj;
+			}	
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+		}
 		return null;
 	}
+
+	@Override
+	public List<Department> findAll() {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			st = conn.prepareStatement("SELECT * FROM department ORDER BY Name");
+			
+			rs = st.executeQuery();
+			
+			List<Department> list = new ArrayList<>();
+			
+			while(rs.next()) {
+				Department obj = new Department();
+				obj.setId(rs.getInt("Id"));
+				obj.setName(rs.getString("Name"));
+				list.add(obj);
+			}
+			return list;
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+	}
+	
+	
 
 }
